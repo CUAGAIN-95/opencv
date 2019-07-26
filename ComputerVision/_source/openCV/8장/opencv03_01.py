310de1bba952e16637b2a567cfdd8104f3ce2b3c
CUAGAIN-95 <52403373+CUAGAIN-95@users.noreply.github.com>
2019-07-27 01:20:38 +0900

    line

--- ComputerVision/_source/openCV/8장/opencv03_01.py
+++ ComputerVision/_source/openCV/8장/opencv03_01.py
@@ -4,18 +4,35 @@ import cv2
 import random
 
 image = cv2.imread("../../../_image/_foxes.jpg")
-image_rot = cv2.rotate(image, rotateCode=90)
+image_rot = cv2.imread("../../../_image/_foxes_rot.jpg")
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_rot = cv2.cvtColor(image_rot, cv2.COLOR_BGR2GRAY)
 
 orb = cv2.ORB_create()
 kp, des = orb.detectAndCompute(gray, None)
+kp_rot, des_rot = orb.detectAndCompute(gray_rot, None)
 
-# sift_obj = cv2.xfeatures2d.SIFT_create()
-# keypoints = sift_obj.detect(gray, None)
+# 기본 파라미터로 BFMatcher 생성
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des, des_rot, k=2)
 
-img = cv2.drawKeypoints(image, kp, None, (255, 0, 0))
-cv2.imwrite("../../../_image/_sift_foxes.jpg", img)
-cv2.imshow("sift_keypoints.jpg", img)
-cv2.waitKey()
+# 비율 테스트
+good = []
+for m, n in matches:
+    if m.distance < 0.4 * n.distance:
+        good.append([m])
+
+# 대응된 키포인트를 섞음
+random.shuffle(good)
+
+# cv2.drawMatchesKnn은 대응 쌍의 리스트들의 리스트를 받는다
+
+
+for i in range(0, len(good) - 1, 10):
+    image_match = cv2.drawMatchesKnn(image, kp, image_rot, kp_rot, good[:i], flags=2, outImg=None)
+    print(i)
+    cv2.imshow("sift_matches", image_match)
+    cv2.waitKey(1000)
+
 cv2.destroyAllWindows()
